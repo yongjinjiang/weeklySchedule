589dde62a2f8a178130c88252eea28739e7161d6
jyj <yjjiangphysics@gmail.com>
2020-08-26 22:13:54 -0500

    ready to deploy

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import { Deck, Slide, Heading } from 'spectacle';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Deck>
+      <Slide>
+        <Heading>Welcome to Spectacle</Heading>
+      </Slide>
+      <Slide>
+        <Heading>Welcome to Spectacle2</Heading>
+      </Slide>
+    </Deck>
   );
 }
-
 export default App;
