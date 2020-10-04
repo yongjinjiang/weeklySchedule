9c613dcf16813f8b649c1a5ad1b81dd7c8eda36d
jagdeepsingh <jagdeepsingh.125k@gmail.com>
2020-10-04 15:33:12 -0500

    Updates

---
+++ precache-manifest.2453297924f7110a8ff46cf6a0bfd6a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3821c2fd49c9d0db4ba0d12e1ec47499",
+    "url": "/mathslides/index.html"
+  },
+  {
+    "revision": "c2c5ae7b49748748ddd9",
+    "url": "/mathslides/static/css/main.0a39a5e5.chunk.css"
+  },
+  {
+    "revision": "62da73ce303688f91353",
+    "url": "/mathslides/static/js/2.bb25c456.chunk.js"
+  },
+  {
+    "revision": "3ad3faef84eb1acd44c604f9bf09b98b",
+    "url": "/mathslides/static/js/2.bb25c456.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2c5ae7b49748748ddd9",
+    "url": "/mathslides/static/js/main.74873623.chunk.js"
+  },
+  {
+    "revision": "b8f8ff0b7e9fecbcf5b4",
+    "url": "/mathslides/static/js/runtime-main.01c671e0.js"
+  }
+]);
